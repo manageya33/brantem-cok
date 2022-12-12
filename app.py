c69878e854b7d27d2e2016725ac719da17ff1498
manageya33 <119881005+manageya33@users.noreply.github.com>
2022-12-13 06:59:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def hello_world():
 #ex https://Leoksu:ghp_147bkkabcdefgh@github.com/Leoksu/Ultroid
 
 
-os.system("git clone https://manageya33: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@https://github.com/manageya33/privatepyroman && cd privatepyroman && pip install requirements.txt && pip install -r resour*/start*/optiona*.txt && python3 -m ProjectMan &")
+os.system("git clone https://manageya33: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@github.com/manageya33/privatepyroman && cd privatepyroman && pip install requirements.txt && pip install -r resour*/start*/optiona*.txt && python3 -m ProjectMan &")
